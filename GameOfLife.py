9d87b97a404e1b4f0ee92397d19702632291ab9f
Yash Karle <yashkarle@gmail.com>
2018-11-10 15:53:52 +0000

    2

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,10 +1,14 @@
 import unittest
 
+def num_neigh(grid, x, y):
+    return 0
+
 
 class TestStringMethods(unittest.TestCase):
 
     def test_neighbours(self):
-        self.assertEqual(1, 1)
+        grid=[]
+        self.assertEqual(num_neigh(grid,0,0), 0)
 
 
 if __name__ == '__main__':
