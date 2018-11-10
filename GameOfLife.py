5fffbd8542aa9b5c2aaa7d4851b7258e24da4112
Yash Karle <yashkarle@gmail.com>
2018-11-10 15:58:35 +0000

    4

--- GameOfLife.py
+++ GameOfLife.py
@@ -2,9 +2,12 @@ import unittest
 
 def num_neigh(grid, x, y):
     iterators = [-1,0,1]
-
-    
-    return 0
+    sum = 0
+    for i in iterators:
+        for j in iterators:
+            if 0<x+i<len(grid) and 0<y+j<len(grid):
+                sum = sum + grid[y+j][x+i]
+    return sum - grid[y][x]
 
 
 class TestStringMethods(unittest.TestCase):
