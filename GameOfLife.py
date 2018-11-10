81efd89fa8cdc668fb0de524e5c1edae3c0cb341
Yash Karle <yashkarle@gmail.com>
2018-11-10 15:51:27 +0000

    1:initial test

---
+++ GameOfLife.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_neighbours(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
